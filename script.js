415377779e18feb5d3107ce9dd07fd3a45fe0235
Annie Pylypchuk <anechkaaaaaaa@gmail.com>
2023-02-08 20:48:30 +0200

    without modules

--- script.js
+++ script.js
@@ -4,18 +4,13 @@ let resultArray = document.getElementById('array');
 let resultSet = document.getElementById('set');
 let emojiInput = document.querySelector('.input-emoji');
 const btn = document.querySelector('.btn');
-let random = 0;
 let emojiiList = ['🐮', '🐯', '🐼', '🐨', '🐵', '🐷', '🐸'];
-//let emojii = ['&#128512', '&#128516', '&#128513', '&#128514'];
-
-
 
 btn.addEventListener('click', function (event) {
 	event.preventDefault();
-	showEmoji();
 	addArray();
 	addSet();
-  
+  	showEmoji();
 });
 
 let arrayOfEmoji = [];
@@ -24,23 +19,20 @@ let setOfEmoji = new Set();
 function addArray() {
 	let val = emojiInput.value;
   	arrayOfEmoji.push(val);
-	resultArray.innerHTML += val;
-
-	console.log('arrayOfEmoji---->',arrayOfEmoji);
-
+	resultArray.innerHTML = `<p>[${arrayOfEmoji} ]</p>`
+	// console.log('arrayOfEmoji---->',arrayOfEmoji);
 }
 
 function addSet() {
-	let value = emojiInput.value
+	let value = emojiInput.value;
   	setOfEmoji.add(value); 
-    resultSet.innerHTML += value;
-    console.log('setOfEmoji----->',setOfEmoji);
+    let myArr = Array.from(setOfEmoji); 
+     resultSet.innerHTML = `<p>[${myArr} ]</p>`
+    // console.log('setOfEmoji----->',setOfEmoji);
 }
 
 function showEmoji(){
-	 random = Math.floor(Math.random() * emojiiList.length);
-	emojiInput.value = emojiiList[random];
-	console.log(emojiiList);
+	 let random = Math.floor(Math.random() * emojiiList.length);
+	 emojiInput.value = emojiiList[random];
 }
 
- // emojiInput.value = emojiiList[random];
